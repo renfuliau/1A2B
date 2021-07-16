2d114c419fc49a405afa4b5d9d6edcaa52f6f9b8
ubuntu <renfuliau@gmail.com>
2021-07-16 18:05:08 +0800

    0716

--- ValidateTheGuess.php
+++ ValidateTheGuess.php
@@ -1,19 +1,54 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8'); //設定資料類型為 json，編碼 utf-8
 
-$regex = '/^(?!\d*?(\d)\d*?\1)\d{4}$/';
+function shuffleFourNumber()
+{
+    $tenNumberArray = range(0, 9);
+    shuffle($tenNumberArray);
+    return $fourNumberArray = array_slice($tenNumberArray, 0, 4);
+}
+
+function parseGuess($guess, $answerArray)
+{
+    $guessArray = str_split($guess);
+    // print_r($guessArray);
+
+    $A = 0;
+    $B = 0;
+
+    foreach ($guessArray as $key => $value) {
+        if ($value == $answerArray[$key]) {
+            $A++;
+            continue;
+        }
+
+        if (in_array($value, $answerArray)) {
+            $B++;
+        }
+    }
+
+    return array($A, $B);
+}
+
+if ($id == 1) {
+    $answer = ShuffleFourNumber();
+}
 $guess = $_POST['guess'];
+$regex = '/^(?!\d*?(\d)\d*?\1)\d{4}$/';
+
+$id = $_POST['id'];
 // echo $guess;
-if (! preg_match($regex, $guess)) {
+if (!preg_match($regex, $guess)) {
     // echo '2';
+    
     echo json_encode(array(
         'guess' => $guess,
-        'boolean' => 0
-    )); 
+        'result' => 0
+    ));
 } else {
     // echo '3';
     echo json_encode(array(
         'guess' => $guess,
-        'boolean' => 1
+        'result' => 1
     ));
 }
