7387f6fe14f7f5772aec86dad152f659633f76b3
ubuntu <renfuliau@gmail.com>
2021-07-19 18:04:16 +0800

    0719-3

---
+++ DB.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2021 年 07 月 19 日 18:02
+-- 伺服器版本： 5.7.34-0ubuntu0.18.04.1
+-- PHP 版本： 7.2.24-0ubuntu0.18.04.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `players`
+--
+
+CREATE TABLE `players` (
+  `id` int(10) NOT NULL,
+  `ip_address` varchar(16) NOT NULL,
+  `play_times` int(10) NOT NULL,
+  `answer` varchar(4) NOT NULL,
+  `created_at` varchar(10) NOT NULL,
+  `edited_at` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `players`
+--
+ALTER TABLE `players`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
